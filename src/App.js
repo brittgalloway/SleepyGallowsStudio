493e9f5479895f5572293b81d892576a7b55b589
BrittGall <crlnfllr@gmail.com>
2020-01-20 01:29:40 -0600

    added routes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
-// import Nav from "./components/Nav";
 import Home from "./pages/Home";
 import Art from "./pages/Art";
 import Crystal from "./pages/Art/Crystal";
@@ -13,14 +12,17 @@ import ClientWork from "./pages/Animation/ClientWork";
 import ForFun from "./pages/Animation/ForFun";
 import Comics from "./pages/Comics";
 import WebDev from "./pages/WebDev";
-import Project from "./pages/WebDev/Project";
-// import AboutWeb from "./pages/AboutWeb";
+import Sgs from "./pages/WebDev/Sgs";
+import Memory from "./pages/WebDev/Memory";
+import SpaceChase from "./pages/WebDev/SpaceChase";
+import Swaddle from "./pages/WebDev/Swaddle";
+import Swiftly from "./pages/WebDev/Swiftly";
+import Tennis from "./pages/WebDev/Tennis";
 
 function App() {
   return (
     <Router>
       <div>
-        {/* <Nav /> */}
         <Route exact path="/" component={Home} />
         <Route exact path="/Art" component={Art} />
         <Route exact path="/Art/Crystal" component={Crystal} />
@@ -31,11 +33,14 @@ function App() {
         <Route exact path="/Animation/clientwork" component={ClientWork} />
         <Route exact path="/Animation/forfun" component={ForFun} />
         <Route exact path="/Comics" component={Comics} />
-        <Switch>
-          <Route exact path="/WebDev" component={WebDev} />
-          <Route exact path="/WebDev/:Project" component={Project} />
-        </Switch>
-        {/* <Route exact path="/WebDev/About" component={AboutWeb} /> */}
+
+        <Route exact path="/WebDev" component={WebDev} />
+        <Route exact path="/WebDev/sgs" component={Sgs} />
+        <Route exact path="/WebDev/Memory" component={Memory} />
+        <Route exact path="/WebDev/SpaceChase" component={SpaceChase} />
+        <Route exact path="/WebDev/Swaddle" component={Swaddle} />
+        <Route exact path="/WebDev/Swiftly" component={Swiftly} />
+        <Route exact path="/WebDev/Tennis" component={Tennis} />
       </div>
     </Router>
   );
