6e69bc7a3197a931191350e0a4307a55e40cab9d
BrittGall <crlnfllr@gmail.com>
2020-01-24 22:36:27 -0600

    home page works for 1536x754 screen(mine haha)

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -5,40 +5,19 @@ import "./style.scss";
 function Home() {
   return (
     <>
-      <div className="columns is-gapless is-multiline is-mobile">
-        <div className="column is-half">
-          <Link to="/Animation">
-            <img
-              alt="animation"
-              src={process.env.PUBLIC_URL + "/images/Rectangle_1animation.png"}
-            />
-          </Link>
-        </div>
-        <div className="column is-half">
-          <Link to="/Art">
-            <img
-              alt="Art"
-              src={process.env.PUBLIC_URL + "/images/Rectangle_2art.png"}
-            />
-          </Link>
-        </div>
-
-        <div className="column is-half">
-          <Link to="/Comics">
-            <img
-              alt="Comics"
-              src={process.env.PUBLIC_URL + "/images/Rectangle_3Comics.png"}
-            />
-          </Link>
-        </div>
-        <div className="column is-half">
-          <Link to="/WebDev">
-            <img
-              alt="WebDev"
-              src={process.env.PUBLIC_URL + "/images/Rectangle_4web.png"}
-            />
-          </Link>
-        </div>
+      <div className="gridContainer">
+        <Link to="/Animation">
+          <div id="animationHome">Animation</div>
+        </Link>
+        <Link to="/Art">
+          <div id="artHome">Art</div>
+        </Link>
+        <Link to="/Comics">
+          <div id="comicHome">Comics</div>
+        </Link>
+        <Link to="/WebDev">
+          <div id="webdevHome">Web Development</div>
+        </Link>
       </div>
     </>
   );
