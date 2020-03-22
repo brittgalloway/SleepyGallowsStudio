02929ecb76ad923f13633964427379e072fd0cee
Brittney Galloway <crlnfllr@gmail.com>
2020-03-21 23:43:15 -0500

    Updates

---
+++ precache-manifest.c2a87f596d0c7e5b3c33b2157bcb48d1.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "438edae4ee36e21a2913261c979df422",
+    "url": "/index.html"
+  },
+  {
+    "revision": "715e732b58d4bef453dd",
+    "url": "/static/css/main.de273e5b.chunk.css"
+  },
+  {
+    "revision": "bda2955f8d1826743e04",
+    "url": "/static/js/2.8b065214.chunk.js"
+  },
+  {
+    "revision": "5bd09eb7f2cf4d145d532d862be18942",
+    "url": "/static/js/2.8b065214.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "715e732b58d4bef453dd",
+    "url": "/static/js/main.623f1d8c.chunk.js"
+  },
+  {
+    "revision": "aceada9465db8a405c78",
+    "url": "/static/js/runtime-main.d16a2cbc.js"
+  },
+  {
+    "revision": "cf4f97bff4a977c638abb0d4572af81e",
+    "url": "/static/media/Rectangle_1animation.cf4f97bf.png"
+  },
+  {
+    "revision": "7bf4495910f6bf8b2fd98ade2ef9161a",
+    "url": "/static/media/Rectangle_2art.7bf44959.png"
+  },
+  {
+    "revision": "76d590c716056f58a467d1dd52d55d69",
+    "url": "/static/media/Rectangle_3Comics.76d590c7.png"
+  },
+  {
+    "revision": "cd0b2e6425fa3e4416e6826ed983829b",
+    "url": "/static/media/Rectangle_4web.cd0b2e64.png"
+  },
+  {
+    "revision": "a41d88baa0689af1f142e798d5d1ae4f",
+    "url": "/static/media/naca_bg.a41d88ba.jpg"
+  }
+]);
