d9a8cfa7f4e8e931514d03ad563bb0f53809b1b1
Brittney Galloway <crlnfllr@gmail.com>
2020-03-21 22:08:08 -0500

    deployed?

---
+++ build/precache-manifest.9dc5894bd1ff681adf5eec288795c0c7.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "76376a2c00e383bb0ce7e9e9e6a223da",
+    "url": "/brittgalloway/SleepyGallowsStudio/index.html"
+  },
+  {
+    "revision": "45afa0038a1ad038e9fd",
+    "url": "/brittgalloway/SleepyGallowsStudio/static/css/main.78374961.chunk.css"
+  },
+  {
+    "revision": "bda2955f8d1826743e04",
+    "url": "/brittgalloway/SleepyGallowsStudio/static/js/2.8b065214.chunk.js"
+  },
+  {
+    "revision": "5bd09eb7f2cf4d145d532d862be18942",
+    "url": "/brittgalloway/SleepyGallowsStudio/static/js/2.8b065214.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "45afa0038a1ad038e9fd",
+    "url": "/brittgalloway/SleepyGallowsStudio/static/js/main.f42764c9.chunk.js"
+  },
+  {
+    "revision": "f834336b51d489789f59",
+    "url": "/brittgalloway/SleepyGallowsStudio/static/js/runtime-main.7ace8f76.js"
+  },
+  {
+    "revision": "cf4f97bff4a977c638abb0d4572af81e",
+    "url": "/brittgalloway/SleepyGallowsStudio/static/media/Rectangle_1animation.cf4f97bf.png"
+  },
+  {
+    "revision": "7bf4495910f6bf8b2fd98ade2ef9161a",
+    "url": "/brittgalloway/SleepyGallowsStudio/static/media/Rectangle_2art.7bf44959.png"
+  },
+  {
+    "revision": "76d590c716056f58a467d1dd52d55d69",
+    "url": "/brittgalloway/SleepyGallowsStudio/static/media/Rectangle_3Comics.76d590c7.png"
+  },
+  {
+    "revision": "cd0b2e6425fa3e4416e6826ed983829b",
+    "url": "/brittgalloway/SleepyGallowsStudio/static/media/Rectangle_4web.cd0b2e64.png"
+  },
+  {
+    "revision": "a41d88baa0689af1f142e798d5d1ae4f",
+    "url": "/brittgalloway/SleepyGallowsStudio/static/media/naca_bg.a41d88ba.jpg"
+  }
+]);
