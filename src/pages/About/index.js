d225e03f916ef448632ae3a7cfe20f897cec95d5
Brittney Galloway <crlnfllr@gmail.com>
2020-03-23 11:31:05 -0500

    try to merge again

---
+++ src/pages/About/index.js
@@ -0,0 +1,41 @@
+import React from "react";
+import { Nav, NavBur } from "../../components/Nav";
+import { AboutSG, AboutCreators, Contact } from "../../components/About";
+import { FooterSleepy } from "../../components/Footer";
+import "./style.scss";
+
+function About() {
+  return (
+    <>
+      <div className="columns is-gapless">
+        <div className="column is-fixed is-narrow">
+          <Nav />
+        </div>
+        <div className="column">
+          <div className="columns">
+            <div className="column is-6 is-offset-3 is-full-mobile is-full-tablet">
+              <NavBur />
+              <div className="aboutsection">
+                <AboutSG />
+              </div>
+              <div className="aboutsection">
+                <AboutCreators />
+              </div>
+            </div>
+
+            <div className="column is-3">
+              <div className="aboutsection">
+                <div className="contactSG">
+                  <Contact />
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <FooterSleepy />
+    </>
+  );
+}
+
+export default About;
