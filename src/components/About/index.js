61c74e84ca3c8aebbbe6d10bd98968f90b016e59
BrittGall <crlnfllr@gmail.com>
2020-04-09 16:39:30 -0500

    commented out btn

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { DonateBtn } from "../Btn";
+// import { DonateBtn } from "../Btn";
 import { Link } from "react-router-dom";
 
 export function AboutCreators() {
